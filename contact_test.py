a34880819f8ae050ee26f8ac5f8415bcad142629
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2024-07-16 14:17:18 +0300

    Add the find contact by number test

--- contact_test.py
+++ contact_test.py
@@ -23,18 +23,27 @@ class TestContact(unittest.TestCase):
 
     def test_save_multiple_contact(self):
         self.new_contact.save_contact()
-        test_contact = Contact("Test","user","0712345678","test@user.com")#new contact
+        test_contact = Contact("Test","user","07111223344","test@user.com")#new contact
 
         test_contact.save_contact()
         self.assertEqual(len(Contact.contact_list),2)
 
     def test_delete_contact(self):
         self.new_contact.save_contact()
-        test_contact = Contact("Test","user","0712345678","test@user.com")#new contact
+        test_contact = Contact("Test","user","07111223344","test@user.com")#new contact
 
         test_contact.save_contact()
         self.new_contact.delete_contact()#Deleting a contact object
         self.assertEqual(len(Contact.contact_list),1)
+
+    def test_find_contact_by_number(self):
+        self.new_contact.save_contact()
+        test_contact = Contact("Test","user","0711223344","test@user.com") # new contact
+
+        test_contact.save_contact()
+        found_contact = Contact.find_by_number("0711223344")
+        self.assertEqual(found_contact.email,test_contact.email)
+
 
 if __name__ == '__main__':
     unittest.main()
