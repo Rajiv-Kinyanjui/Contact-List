c71b482a42a6bb70dd3fb350b510137966506a81
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2024-07-16 13:14:54 +0300

    Add the init method to help define the objects

--- contact.py
+++ contact.py
@@ -2,4 +2,13 @@ class Contact:
     """
     class that generates new instances of contacts
     """
-    pass
+    def __init__(self, first_name,last_name,phone_number,email):
+        """
+        the init method helps us define properties for our objects
+
+        Args:
+            first_name
+            last_name
+            number
+            email
+        """
