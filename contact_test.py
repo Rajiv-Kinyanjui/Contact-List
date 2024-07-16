e85270191377b5c9d6af66f534b424ff94a149a2
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2024-07-16 14:03:40 +0300

    Add the tear down function taht cleans up after eact test case has been run

--- contact_test.py
+++ contact_test.py
@@ -15,6 +15,12 @@ class TestContact(unittest.TestCase):
         self.new_contact.save_contact() #saving the new contact
         self.assertEqual(len(Contact.contact_list),1)
 
+    def tearDown(self):
+        """
+        tearDown method that does clean up after each test case has run.
+        """
+        Contact.contact_list = []
+
     def test_save_multiple_contact(self):
         self.new_contact.save_contact()
         test_contact = Contact("Test","user","0712345678","test@user.com")#new contact
